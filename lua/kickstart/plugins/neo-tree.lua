8bed73b99126f2fec84ce27eda75e19669ee9add
yottta <andreic9203@gmail.com>
2025-05-11 21:42:55 +0300

    Upgrade kickstart to it's latest version to fix also the mason v2 integration
    Signed-off-by: yottta <andreic9203@gmail.com>

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '<leader>ts', ':Neotree toggle<CR>', desc = 'Open [N]eo[T]ree', silent = true },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = { enabled = true, leave_dirs_open = true },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
