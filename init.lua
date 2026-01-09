cd654926636d967027e5718b5873cff439a7e05b
Andrei Ciobanu <andreic9203@gmail.com>
2026-01-09 23:03:09 +0200

    Add tofu-ls to lsp configuration
    Signed-off-by: Andrei Ciobanu <andreic9203@gmail.com>

--- init.lua
+++ init.lua
@@ -628,6 +628,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        tofu_ls = {},
 
         lua_ls = {
           -- cmd = { ... },
